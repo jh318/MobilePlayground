cede6662af85d5c1bfa695814ce8e4a82b810c4b
John Hawley <jhawley@institute.local>
2017-09-01 17:57:04 -0500

    completed gyro/camera thing. doesn't work on my android thing. no gyro I think

--- Assets/GyroTest.cs
+++ Assets/GyroTest.cs
@@ -1,14 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GyroTest : MonoBehaviour {
 
-	void Start(){
+	public RawImage target;
+
+	IEnumerator Start(){
 		Input.gyro.enabled = true;
+		yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
+		if(Application.HasUserAuthorization(UserAuthorization.WebCam)){
+			WebCamTexture tex = new WebCamTexture();
+			target.texture = tex;
+			tex.Play();
+		}
 	}
 
 	void Update(){
 		transform.rotation = Input.gyro.attitude;
+		transform.Rotate(0,0,180, Space.Self);
+		transform.Rotate(90,180,0,Space.World);
 	}
 }
