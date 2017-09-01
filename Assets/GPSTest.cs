43c338f89a44526bfaba1e90182f26665cc40044
John Hawley <jhawley@institute.local>
2017-09-01 18:23:05 -0500

    added GPS script/scene. not sure if current device has GPS.

---
+++ Assets/GPSTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPSTest : MonoBehaviour {
+
+	IEnumerator Start(){
+		if(!SystemInfo.supportsLocationService){
+			Debug.Log("No GPS.");
+			yield break;
+		}
+		else if(!Input.location.isEnabledByUser){
+			Debug.Log("Enabled GPS please.");
+			yield break;
+		}
+
+		while(SystemInfo.supportsLocationService && Input.location.isEnabledByUser){
+			yield return new WaitForSecondsRealtime(0.5f); 
+			switch(Input.location.status){
+				case LocationServiceStatus.Running:
+					float lat = Input.location.lastData.latitude;
+					float lon = Input.location.lastData.longitude;
+					Debug.Log("LAT: " + lat + ", LON: " + lon);
+					break;
+				case LocationServiceStatus.Stopped:
+					Input.location.Start();
+					break;
+				default:
+					break;
+			}
+		}
+	}
+}
